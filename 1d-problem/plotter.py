ce6a1325e1662e8199c751d0efe6a03c94caf161
Nathan Glaser <glaserna@oregonstate.edu>
2026-08-09 22:00:14 -0700

    updated plotter

--- 1d-problem/plotter.py
+++ 1d-problem/plotter.py
@@ -18,16 +18,24 @@ with h5py.File("sde_output.h5", "r") as f:
     SDE_DS = [ds for ds in SDE_DS if ds]
 
 fig, ax = plt.subplots()
+err_fig, err_ax = plt.subplots()
 for ds_name in SDE_DS:
   with h5py.File("sde_output.h5", "r") as f:
     group = f[ds_name]
     x = group["x_centers"][()]
     phi = group["phi"][()]
   ax.plot(x, phi, label =f"DS={ds_name[3:]}")
+  err_ax.plot(x, (phi - REF_PHI) / REF_PHI, label=f"DS={ds_name[3:]}")
 
 ax.plot(REF_X, REF_PHI, label="Reference")
 ax.axvline(0.75, color="k", ls="--",label="Material Interface")
+err_ax.axvline(0.75, color="k", ls="--",label="Material Interface")
 ax.legend()
+err_ax.legend()
 ax.set_xlabel(r"X Position  $\left[cm\right]$")
+err_ax.set_xlabel(r"X Position  $\left[cm\right]$")
 ax.set_ylabel(r"$\phi$  $\left[cm^{2}\cdot s^{-1}\right]$")
+err_ax.set_ylabel("Relative Error (to reference)")
+fig.savefig("phi")
+err_fig.savefig("error")
 plt.show()
