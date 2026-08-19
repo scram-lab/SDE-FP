c18068103907d8b302ce3fab5b0c30b3639ca7ca
Nathan Glaser <glaserna@oregonstate.edu>
2026-08-19 09:38:37 -0700

    added fom and error plots, select ds

--- 1d-problem/plotter.py
+++ 1d-problem/plotter.py
@@ -1,8 +1,11 @@
+import pathlib
 import h5py
 import matplotlib.pyplot as plt
 import pandas as pd 
 import numpy as np
 
+SAVE_PATH = pathlib.Path("figures")
+SAVE_PATH.mkdir(exist_ok=True)
 # Reference
 REF_SOLN_DICT = {}
 with h5py.File("reference_sn.h5", "r") as f:
@@ -32,34 +35,63 @@ with h5py.File("sde_output.h5", "r") as sde_file:
             }
             SDE_DS_2_SCALE[float(ds)] = ds_sets["scale"][()][0]
 
+SDE_DS = sorted(SDE_DS, reverse=True)
+select_DS = [SDE_DS[i] for i in range(len(SDE_DS)) if not i % (len(SDE_DS)/2)] + [SDE_DS[-1]]
+
 fig, ax = plt.subplots()
-ds_err_plot = {ds: plt.subplots() for ds in SDE_DS}
-for method, ds_dict in SDE_SOLN_DICT.items():
-    for ds, soln_dict in ds_dict.items():
-        ax.plot(soln_dict["x"], soln_dict["phi"], label=f"{method}, DS={ds}")
-        ds_err_plot[ds][1].plot(soln_dict["x"], (soln_dict["phi"] - REF_PHI) / REF_PHI, label=method)
-        ds_err_plot[ds][1].set_title(rf"$\Delta_{{s}}$ = {SDE_DS_2_SCALE[ds]}$\cdot\Sigma_{{tr}}$")
+ds_err_plot = {ds: plt.subplots() for ds in select_DS}
+error_array = np.zeros((len(SDE_SOLN_DICT), len(SDE_DS)))
+for i, (method, ds_dict) in enumerate(SDE_SOLN_DICT.items()):
+    for j, ds in enumerate(SDE_DS):
+        x = ds_dict[ds]["x"]
+        phi = ds_dict[ds]["phi"]
+        if ds in select_DS:
+            ax.plot(x, phi, label=f"{method}, DS={ds}")
+            ds_err_plot[ds][1].plot(x, (phi - REF_PHI) / REF_PHI, label=method)
+            ds_err_plot[ds][1].set_title(rf"$\Delta_{{s}}$ = {SDE_DS_2_SCALE[ds]}$\cdot\lambda_{{\text{{mfp,tr}}}}$")
+        error_array[i, j] = np.linalg.norm(phi - REF_PHI, np.inf) / np.linalg.norm(REF_PHI, np.inf)
+df = pd.DataFrame(error_array, index=[*SDE_SOLN_DICT.keys()], columns=[str(SDE_DS_2_SCALE[ds]) for ds in SDE_DS])
+df.columns.name = "DS Scale"
+print(df)
 
 ax.plot(REF_X, REF_PHI, label="Reference")
 ax.axvline(0.75, color="k", ls="--", label="Material Interface")
 ax.legend()
 ax.set_xlabel(r"X Position  $\left(cm\right)$")
 ax.set_ylabel(r"$\phi$  $\left(cm^{2}\cdot s^{-1}\right)$")
-fig.savefig("phi")
+fig.savefig(SAVE_PATH / "phi")
 
-for ds, (fig, ax) in ds_err_plot.items():
+for ds in select_DS:
+    fig, ax = ds_err_plot[ds]
     ax.axvline(0.75, color="k", ls="--", label="Material Interface")
     ax.legend()
     ax.set_xlabel(r"X Position  $\left(cm\right)$")
     ax.set_ylabel("Relative Error (to reference)")
-    fig.savefig(f"error_{SDE_DS_2_SCALE[ds]}.pdf")
-plt.show()
+    fig.savefig(SAVE_PATH / f"error_{SDE_DS_2_SCALE[ds]}.pdf")
+    if ds not in select_DS: plt.close(fig)
 
 time_array = np.zeros((len(SDE_SOLN_DICT), len(SDE_DS)))
 for i, (method, ds_dict) in enumerate(SDE_SOLN_DICT.items()):
-    for j, (ds, subdict) in enumerate(ds_dict.items()):
-        time_array[i, j] = subdict["time"]
-df = pd.DataFrame(time_array, index=[*SDE_SOLN_DICT.keys()], columns=[str(SDE_DS_2_SCALE[ds]) for ds in ds_dict.keys()])
+    for j, ds in enumerate(SDE_DS):
+        time_array[i, j] = ds_dict[ds]["time"]
+df = pd.DataFrame(time_array, index=[*SDE_SOLN_DICT.keys()], columns=[str(SDE_DS_2_SCALE[ds]) for ds in SDE_DS])
 df.columns.name = "DS Scale"
 print(df)
-        
+
+scales = [float(SDE_DS_2_SCALE[ds]) for ds in SDE_DS]
+err_fig, err_ax = plt.subplots()
+fom_fig, fom_ax = plt.subplots()
+for i, method in enumerate(SDE_SOLN_DICT.keys()):
+    times = time_array[i]
+    errors = error_array[i]
+    err_ax.plot(scales,  errors, label=method)
+    fom_ax.semilogy(scales, 1/ (times * errors**2), label=method)
+for ax in [err_ax, fom_ax]:
+    ax.legend()
+    ax.set_xlabel(r"$\Delta_s$ scale to $\lambda_{{\text{{mfp,tr}}}}$")
+err_ax.set_ylabel("Relative Error (to reference)")
+err_fig.savefig(SAVE_PATH/"rel_err")
+fom_ax.set_ylabel(r"Figure of Merit  $\left[\frac{1}{t_{r}\sigma_{r}^{2}}\right]$")
+fom_fig.savefig(SAVE_PATH/"fom")
+
+plt.show()
